95f7d9e3714e66c83d19a73f96347cf12a96acc0
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-03-24 13:49:39 -0500

    update generator

--- api/forms.py
+++ api/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from api.models import UserProfile
+from PIL import Image
 
 class GenerateForm(forms.Form):
     #Platform
-    platform = forms.ChoiceField(choices=[('windows','Windows'),('linux','Linux (currently unavailable)'),('android','Android (testing now available)')], initial='windows')
-    version = forms.ChoiceField(choices=[('master','nightly'),('1.3.6','1.3.6'),('1.3.5','1.3.5'),('1.3.4','1.3.4'),('1.3.3','1.3.3'),('1.3.2','1.3.2'),('1.3.1','1.3.1'),('1.3.0','1.3.0')], initial='1.3.6')
+    platform = forms.ChoiceField(choices=[('windows','Windows'),('linux','Linux (currently unavailable)'),('android','Android'),('macos','macOS')], initial='windows')
+    version = forms.ChoiceField(choices=[('master','nightly'),('1.3.8','1.3.8'),('1.3.7','1.3.7'),('1.3.6','1.3.6'),('1.3.5','1.3.5'),('1.3.4','1.3.4'),('1.3.3','1.3.3')], initial='1.3.8')
+    help_text="'master' is the development version (nightly build) with the latest features but may be less stable"
     delayFix = forms.BooleanField(initial=True, required=False)
 
     #General
@@ -29,10 +31,14 @@ class GenerateForm(forms.Form):
     apiServer = forms.CharField(label="API Server", required=False)
     key = forms.CharField(label="Key", required=False)
     urlLink = forms.CharField(label="Custom URL for links", required=False)
+    downloadLink = forms.CharField(label="Custom URL for downloading new versions", required=False)
+    compname = forms.CharField(label="Company name",required=False)
 
     #Visual
     iconfile = forms.FileField(label="Custom App Icon (in .png format)", required=False, widget=forms.FileInput(attrs={'accept': 'image/png'}))
     logofile = forms.FileField(label="Custom App Logo (in .png format)", required=False, widget=forms.FileInput(attrs={'accept': 'image/png'}))
+    iconbase64 = forms.CharField(required=False)
+    logobase64 = forms.CharField(required=False)
     theme = forms.ChoiceField(choices=[
         ('light', 'Light'),
         ('dark', 'Dark'),
@@ -72,6 +78,33 @@ class GenerateForm(forms.Form):
     cycleMonitor = forms.BooleanField(initial=False, required=False)
     xOffline = forms.BooleanField(initial=False, required=False)
     hidecm = forms.BooleanField(initial=False, required=False)
+    removeNewVersionNotif = forms.BooleanField(initial=False, required=False)
+
+    def clean_iconfile(self):
+        print("checking icon")
+        image = self.cleaned_data['iconfile']
+        if image:
+            try:
+                # Open the image using Pillow
+                img = Image.open(image)
+
+                # Check if the image is a PNG (optional, but good practice)
+                if img.format != 'PNG':
+                    raise forms.ValidationError("Only PNG images are allowed.")
+
+                # Get image dimensions
+                width, height = img.size
+
+                # Check for square dimensions
+                if width != height:
+                    raise forms.ValidationError("Custom App Icon dimensions must be square.")
+                
+                return image
+            except OSError:  # Handle cases where the uploaded file is not a valid image
+                raise forms.ValidationError("Invalid icon file.")
+            except Exception as e: # Catch any other image processing errors
+                raise forms.ValidationError(f"Error processing icon: {e}")
+
 
 class AddPeerForm(forms.Form):
     clientID = forms.CharField(label="Client Rustdesk ID", required=True)
